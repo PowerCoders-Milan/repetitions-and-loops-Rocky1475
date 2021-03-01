03dd249bdcd5222abb65b8833dd9606069972648
Muhammad rRehman <muhammadislamian@gmail.com>
2021-03-01 14:48:01 +0000

    ex 2 half done

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -1,7 +1,18 @@
 // odd or even reporter
 
 // write a for loop to iterate from 0 to 20
+for (i = 0; i < 21; i++){
+  console.log(i);
+}
 
   // check if the current is even or odd
+  a = prompt("inter a number")
+  console.log ("Number :")
+  int = a
+    if a = (a%2 == 0){
+      console.log (a + "is an even number");
+        else {
+          console.log (a + "is an odd number");
+        }
 
   // report the result to the screen
